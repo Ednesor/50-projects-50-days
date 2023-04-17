dae021d3db46797a3e014ca291f43344102cd86d
Ednesor <edgar.funes2001@gmail.com>
2023-04-17 18:27:11 -0300

    change type of file

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import { Inter } from 'next/font/google'
+import Layout from '@/components/Layout/Layout'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <Layout>
+      <main>
+        
+      </main>
+    </Layout>
+  )
+}
