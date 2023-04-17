921d20d1eeb78b07f8ea091de2b932a38b1e8c73
Ednesor <edgar.funes2001@gmail.com>
2023-04-17 20:49:23 -0300

    fix: fixed links and icons

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,6 +3,8 @@ import style from "./Footer-style.module.css"
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
 
 export default function Footer() {
     return (
@@ -15,14 +17,14 @@ export default function Footer() {
                     </Link>
                 </li>
                 <li className={style.item}>
-                    <Link className={style.link} href="https://github.com/Ednesor" passHref={true} target='_blank'>
-                        <FontAwesomeIcon className={style.icon} icon={faGithub} />
+                    <Link className={style.link} href="https://portfolio-ednesor.vercel.app" passHref={true} target='_blank'>
+                        <FontAwesomeIcon className={style.icon} icon={faBriefcase} />
                         <span className={style.name}>Portfolio</span>
                     </Link>
                 </li>
                 <li className={style.item}>
-                    <Link className={style.link} href="https://github.com/Ednesor" passHref={true} target='_blank'>
-                        <FontAwesomeIcon className={style.icon} icon={faGithub} />
+                    <Link className={style.link} href="https://www.linkedin.com/in/edgardo-ibar-funes-ortiz-54ba41217/" passHref={true} target='_blank'>
+                        <FontAwesomeIcon className={style.icon} icon={faLinkedin} />
                         <span className={style.name}>LinkeIn</span>
                     </Link>
                 </li>
