67c1ca09e75f9b476f5f0d0f32f1fb9f17999d2d
Ednesor <edgar.funes2001@gmail.com>
2023-04-17 20:37:56 -0300

    feat: add footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import style from "./Footer-style.module.css"
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default function Footer() {
-  return (
-    <footer>
-        <h1>footer</h1>
-    </footer>
-  )
+    return (
+        <footer className={style.container}>
+            <ul className={style.list}>
+                <li className={style.item}>
+                    <Link className={style.link} href="https://github.com/Ednesor" passHref={true} target='_blank'>
+                        <FontAwesomeIcon className={style.icon} icon={faGithub} />
+                        <span className={style.name}>Github</span>
+                    </Link>
+                </li>
+                <li className={style.item}>
+                    <Link className={style.link} href="https://github.com/Ednesor" passHref={true} target='_blank'>
+                        <FontAwesomeIcon className={style.icon} icon={faGithub} />
+                        <span className={style.name}>Portfolio</span>
+                    </Link>
+                </li>
+                <li className={style.item}>
+                    <Link className={style.link} href="https://github.com/Ednesor" passHref={true} target='_blank'>
+                        <FontAwesomeIcon className={style.icon} icon={faGithub} />
+                        <span className={style.name}>LinkeIn</span>
+                    </Link>
+                </li>
+            </ul>
+            <p className={style.copyright}>Copyright Ednesor 2023</p>
+        </footer>
+    )
 }
