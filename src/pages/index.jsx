e968ecf8098fce8183c5bb515a21b6e9076f35d0
Ednesor <edgar.funes2001@gmail.com>
2023-04-20 02:06:48 -0300

    style: flashlight effect(no finished)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,18 @@
 import { Inter } from 'next/font/google'
+import style from "../styles/index.module.css"
 import Layout from '@/components/Layout/Layout'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Layout>
-      
+      <div className={style.container}>
+        <div className={style.flashlight}></div>
+        <h1 className={style.title}>Welcome to my <span className={style.subtitle}>personal challenge</span></h1>
+        <Link className={style.button} href={"/index"}>Get started</Link>
+      </div>
     </Layout>
   )
 }
