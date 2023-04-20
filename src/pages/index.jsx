138be772bc89d937964a4beae9ce7d8c2ddd5c84
Ednesor <edgar.funes2001@gmail.com>
2023-04-19 23:00:07 -0300

    refactor

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,12 @@
 import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout/Layout'
-import ExpandingCards from '@/components/expanding-cards/ExpandingCards'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Layout>
-      <main>
-        <ExpandingCards />
-      </main>
+      
     </Layout>
   )
 }
