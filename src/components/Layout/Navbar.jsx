6499a1cddcb7498464933ef21cffe4dd2f6c582d
Ednesor <edgar.funes2001@gmail.com>
2023-04-19 23:02:56 -0300

    feat: add link to name

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -5,12 +5,22 @@ import Link from 'next/link'
 export default function Navbar() {
   return (
     <nav className={style.container}>
-        <span className={style.name}><span>&lt;</span>Edgardo Funes<span>/&gt;</span></span>
-        <ul className={style.ul_container}>
-            <li><Link href={"/about"}>About</Link></li>
-            <li><Link href={"/index"}>Index</Link></li>
-            <li><Link href={"/contact"}>Contact</Link></li>
-        </ul>
+      <Link href={"/"}>
+        <span className={style.name}>
+          <span>
+            &lt;
+          </span>
+          Edgardo Funes
+          <span>
+            /&gt;
+          </span>
+        </span>
+      </Link>
+      <ul className={style.ul_container}>
+        <li><Link href={"/about"}>About</Link></li>
+        <li><Link href={"/index"}>Index</Link></li>
+        <li><Link href={"/contact"}>Contact</Link></li>
+      </ul>
     </nav>
   )
 }
