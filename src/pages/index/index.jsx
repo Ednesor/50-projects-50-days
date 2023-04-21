0e0272ef2667c73d0f57c3babdf568bb2c84ad75
Ednesor <edgar.funes2001@gmail.com>
2023-04-21 20:36:51 -0300

    feat: page finished

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,11 +1,28 @@
 import Layout from '@/components/Layout/Layout'
 import React from 'react'
 import style from "./index.module.css"
+import IndexCards from '@/components/IndexCards/IndexCards'
 
 export default function Index() {
   return (
     <Layout title={"Index | 50 projects 50 days"}>
-        <h1>Indice</h1>
+      <h1 className={style.mainTitle}>All projects</h1>
+      <div className={style.container}>
+        <IndexCards className={style.cards}
+          device={"both"}
+          date={["01", "may"]}
+          title={"Proyecto 1"}
+          description={"Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD. amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD.amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD, Este proyecto fue realizado con mucho amor XD."}
+          url={"proyecto-formato-url"}
+        />
+        <IndexCards
+          device={"both"}
+          date={["01", "may"]}
+          title={"Proyecto 1"}
+          description={"Este proyecto fue realizado con mucho amor XD"}
+          url={"proyecto-formato-url"}
+        />
+      </div>
     </Layout>
   )
 }
