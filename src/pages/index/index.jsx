92d8c934ae948295c51dc98c5d83350aa665b5c3
Ednesor <edgar.funes2001@gmail.com>
2023-04-26 00:06:14 -0300

    feat: add new component

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -15,6 +15,13 @@ export default function Index() {
           description={"This component allows you to preview several images by expanding the ones you want to see"}
           url={"expanding-cards"}
         />
+        <IndexCards className={style.cards}
+          device={"desktop"}
+          date={["25", "april"]}
+          title={"Progress Steps"}
+          description={"This component allows to move in a menu through steps"}
+          url={"progress-steps"}
+        />
       </div>
     </Layout>
   )
