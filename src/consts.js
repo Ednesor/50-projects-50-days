22b7e3b21a3c76c6cbfe7e4c0ab0c264a3eba081
Ednesor <edgar.funes2001@gmail.com>
2023-05-04 14:22:16 -0300

    refactor: generate a component for each const

--- src/consts.js
+++ src/consts.js
@@ -5,3 +5,55 @@ export const HIDDEN_SEARCH = "hidden-search";
 export const BLURRY_LOADING = "blurry-loading";
 export const SCROLL_ANIMATION = "scroll-animation";
 export const SPLIT_LANDING = "split-landing";
+
+export const components = [
+    {
+        title: "Expanding Cards",
+        device: "desktop",
+        date: ["24", "april"],
+        description: "This component allows you to preview several images by expanding the ones you want to see",
+        url: "expanding-cards"
+    },
+    {
+        device: "desktop",
+        date: ["25", "april"],
+        title: "Progress Steps",
+        description: "This component allows to move in a menu through steps",
+        url: PROGRESS_STEPS,
+    },
+    {
+        device: "desktop",
+        date: ["26", "april"],
+        title: "Rotating Navigation Animation",
+        description: "A very nice and striking navbar",
+        url: ROTATING_NAVIGATION,
+    },
+    {
+        device: "both",
+        date: ["27", "april"],
+        title: "Hidden search",
+        description: "A search bar that expands and collapses",
+        url: HIDDEN_SEARCH,
+    },
+    {
+        device: "both",
+        date: ["28", "april"],
+        title: "Blurry Loading",
+        description: "A page load component that turns its content from blurry to sharp depending on the load percentage",
+        url: BLURRY_LOADING,
+    },
+    {
+        device: "desktop",
+        date: ["01", "may"],
+        title: "Scroll animation",
+        description: "Cards that are displayed according to the scroll",
+        url: SCROLL_ANIMATION,
+    },
+    {
+        device: "desktop",
+        date: ["02", "may"],
+        title: "Split landing page",
+        description: "A landing page with a nice animation for two images",
+        url: SPLIT_LANDING,
+    },
+];
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -11,61 +11,22 @@ import {
   SCROLL_ANIMATION,
   SPLIT_LANDING
 } from "../../consts"
+import { components } from "../../consts"
 
 export default function Index() {
   return (
     <Layout title={"Index | 50 projects 50 days"}>
       <h1 className={style.mainTitle}>All projects</h1>
       <div className={style.container}>
-        <IndexCards className={style.cards}
-          device={"desktop"}
-          date={["24", "april"]}
-          title={"Expanding Cards"}
-          description={"This component allows you to preview several images by expanding the ones you want to see"}
-          url={EXPADING_CARDS}
+        {components.map((_, i) => (
+          <IndexCards className={style.cards}
+          device={components[i].device}
+          date={components[i].date}
+          title={components[i].title}
+          description={components[i].description}
+          url={components[i].url}
         />
-        <IndexCards className={style.cards}
-          device={"desktop"}
-          date={["25", "april"]}
-          title={"Progress Steps"}
-          description={"This component allows to move in a menu through steps"}
-          url={PROGRESS_STEPS}
-        />
-        <IndexCards className={style.cards}
-          device={"desktop"}
-          date={["26", "april"]}
-          title={"Rotating Navigation Animation"}
-          description={"A very nice and striking navbar"}
-          url={ROTATING_NAVIGATION}
-        />
-        <IndexCards className={style.cards}
-          device={"both"}
-          date={["27", "april"]}
-          title={"Hidden search"}
-          description={"A search bar that expands and collapses"}
-          url={HIDDEN_SEARCH}
-        />
-        <IndexCards className={style.cards}
-          device={"both"}
-          date={["28", "april"]}
-          title={"Blurry Loading"}
-          description={"A page load component that turns its content from blurry to sharp depending on the load percentage"}
-          url={BLURRY_LOADING}
-        />
-        <IndexCards className={style.cards}
-          device={"both"}
-          date={["01", "may"]}
-          title={"Scroll animation"}
-          description={"Cards that are displayed according to the scroll"}
-          url={SCROLL_ANIMATION}
-        />
-        <IndexCards className={style.cards}
-          device={"desktop"}
-          date={["02", "may"]}
-          title={"Split landing page"}
-          description={"A landing page with a nice animation for two images"}
-          url={SPLIT_LANDING}
-        />
+        ))}
       </div>
     </Layout>
   )
