cee6af24d8bcc9171258c172d6f1d2425c456ced
Ednesor <edgar.funes2001@gmail.com>
2023-05-04 14:04:29 -0300

    feat: add new component

--- src/consts.js
+++ src/consts.js
@@ -3,23 +3,5 @@ export const PROGRESS_STEPS = "progress-steps";
 export const ROTATING_NAVIGATION = "rotating-navigation";
 export const HIDDEN_SEARCH = "hidden-search";
 export const BLURRY_LOADING = "blurry-loading";
-
-
-
-/*
-case "expanding-cards":
-      component = <ExpandingCards />;
-      break;
-    case "progress-steps":
-      component = <ProgressSteps />;
-      break;
-    case "rotating-navigation":
-      component = <RotatingNavigation />;
-      break;
-    case "hidden-search":
-      component = <HiddenSearch />;
-      break;
-    case "blurry-loading":
-      component = <HiddenSearch />;
-      break;
-*/
+export const SCROLL_ANIMATION = "scroll-animation";
+export const SPLIT_LANDING = "split-landing";
