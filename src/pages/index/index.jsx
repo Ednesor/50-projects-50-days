45ccb951f213997b62bf30a5f816409499435869
Ednesor <edgar.funes2001@gmail.com>
2023-05-04 14:23:35 -0300

    refactor: I deleted unused code

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -2,15 +2,6 @@ import Layout from '@/components/Layout/Layout'
 import React from 'react'
 import style from "./index.module.css"
 import IndexCards from '@/components/IndexCards/IndexCards'
-import {
-  EXPADING_CARDS,
-  PROGRESS_STEPS,
-  ROTATING_NAVIGATION,
-  HIDDEN_SEARCH,
-  BLURRY_LOADING,
-  SCROLL_ANIMATION,
-  SPLIT_LANDING
-} from "../../consts"
 import { components } from "../../consts"
 
 export default function Index() {
