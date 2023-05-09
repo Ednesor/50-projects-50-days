cf36cd3c50025f27873e71b04cdddee4156125a2
Ednesor <edgar.funes2001@gmail.com>
2023-05-08 23:27:44 -0300

    add file-loader of mp3/wav to the webpack config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,23 @@ const nextConfig = {
   images: {
     domains: ['fastly.picsum.photos', 'images.unsplash.com'],
   },
-}
+  webpack: (config, { isServer }) => {
+    // Configuración del loader para archivos de audio
+    config.module.rules.push({
+      test: /\.(mp3|wav)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/',
+          outputPath: `${isServer ? '../' : ''}static/`,
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
